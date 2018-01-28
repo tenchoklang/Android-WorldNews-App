62ec4f3b28cb7e10c8814adfc433f11ce2555cb8
Tenzin Choklang <tenchoklang@gmail.com>
2018-01-28 00:42:29 -0500

    fifth commit

--- app/src/main/java/com/android/tenchoklang/worldnews/MainActivity.java
+++ app/src/main/java/com/android/tenchoklang/worldnews/MainActivity.java
@@ -3,34 +3,42 @@
 package com.android.tenchoklang.worldnews;
 
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.util.Log;
-import android.view.View;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class MainActivity extends AppCompatActivity implements GetNewsJsonData.OnDataAvailable {
 
 
     private static final String TAG = "MainActivity";
+    private RecyclerViewAdapter recyclerViewAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
+//        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+//        setSupportActionBar(toolbar);
+//
+//        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+//        fab.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//
+//            }
+//        });
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
+        RecyclerView recyclerView = (RecyclerView) findViewById(R.id.recyclerView);
+        //recyler view doesnt take care of handling the layouts, thats done by the layoutManager
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
 
-            }
-        });
+        recyclerViewAdapter = new RecyclerViewAdapter(this, new ArrayList<NewsDetail>());
+        recyclerView.setAdapter(recyclerViewAdapter);
 
         GetNewsJsonData getNewsJsonData = new GetNewsJsonData(this,"https://newsapi.org/v2/top-headlines", true, "us");
         getNewsJsonData.execute("trump");
@@ -45,7 +53,8 @@ public class MainActivity extends AppCompatActivity implements GetNewsJsonData.O
     public void onDataAvailable(List<NewsDetail> newsDetails, DownloadStatus status) {
         Log.d(TAG, "onDataAvailable: Starts");
         if(status == DownloadStatus.OK){
-            Log.d(TAG, "onDataAvailable: ");
+            Log.d(TAG, "onDataAvailable: Loading new data");
+            recyclerViewAdapter.loadNewData(newsDetails);
         }else{
             Log.e(TAG, "onDataAvailable: Failed with status " + status);
         }
--- app/src/main/java/com/android/tenchoklang/worldnews/RecyclerViewAdapter.java
+++ app/src/main/java/com/android/tenchoklang/worldnews/RecyclerViewAdapter.java
@@ -1,21 +1,58 @@
 package com.android.tenchoklang.worldnews;
 
+import android.content.Context;
 import android.support.v7.widget.RecyclerView;
 import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.squareup.picasso.Picasso;
+
+import java.util.List;
 
 /**
  * Created by tench on 1/26/2018.
  */
 
-class RecyclerViewAdapter extends RecyclerView.Adapter{
+class RecyclerViewAdapter extends RecyclerView.Adapter<RecyclerViewAdapter.NewsDetailViewHolder>{
 
     private static final String TAG = "RecyclerViewAdapter";
+    private List<NewsDetail> newsDetailList;
+    private Context context;
+
+    static class NewsDetailViewHolder extends RecyclerView.ViewHolder{
+        private static final String TAG = "FlickrImageViewHolder";
+        TextView title = null;
+        TextView datePublished = null;
+        ImageView thumbnail = null;
+        TextView description = null;
+
+        public NewsDetailViewHolder(View viewForRecycler) {//itemView is the inflatedLayout
+            super(viewForRecycler);
+            Log.d(TAG, "FlickrImageViewHolder: Constructor called");
+            //this.title = (TextView) viewForRecycler.findViewById(R.id.title);
+            //this.datePublished = (TextView) viewForRecycler.findViewById(R.id.datePublished);
+            this.thumbnail = (ImageView) viewForRecycler.findViewById(R.id.thumbnail);
+            //this.description = (TextView) viewForRecycler.findViewById(R.id.description);
+        }
+    }
+
+
+
+    public RecyclerViewAdapter(Context context, List<NewsDetail> newsDetailList){
+        this.context = context;
+        this.newsDetailList = newsDetailList;
+    }
 
     //This inflates a view from whatever layout we want to be displayed in the recyclerView
     @Override
-    public RecyclerView.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
-        return null;
+    public NewsDetailViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        Log.d(TAG, "onCreateViewHolder: New View created");
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.view_for_recycler,parent,false);
+        return new NewsDetailViewHolder(view);
     }
 
 
@@ -26,8 +63,17 @@ class RecyclerViewAdapter extends RecyclerView.Adapter{
     //what we have to do in this method is get that item from the data and put its values into the
     //Viewholder widget
     @Override
-    public void onBindViewHolder(RecyclerView.ViewHolder holder, int position) {
-        Log.d(TAG, "onBindViewHolder: New View Requested");
+    public void onBindViewHolder(NewsDetailViewHolder holder, int position) {
+        //called by the layout manager when it wants new data in a recycled view
+        Log.d(TAG, "onBindViewHolder: New View Data Requested");
+        NewsDetail  newNewsDetail= newsDetailList.get(position);
+
+       // holder.title.setText(newNewsDetail.getTitle());
+       // holder.datePublished.setText(newNewsDetail.getDatePublished());
+        Picasso.with(context).load(newNewsDetail.getUrlToImage()).placeholder(R.drawable.placeholder).into(holder.thumbnail);
+        //holder.description.setText(newNewsDetail.getDescription());
+
+        Log.d(TAG, "onBindViewHolder: " + newNewsDetail.getTitle() + "-->" + position);
 
     }
 
@@ -35,6 +81,12 @@ class RecyclerViewAdapter extends RecyclerView.Adapter{
     @Override
     public int getItemCount() {
         Log.d(TAG, "getItemCount: Called");
-        return 0;
+        return (newsDetailList != null && newsDetailList.size() !=0) ? newsDetailList.size(): 0;
+    }
+
+    //this method will be called once the download is finished, from the MainActivity
+    public void loadNewData(List<NewsDetail> newsDetailList){
+        this.newsDetailList = newsDetailList;
+        notifyDataSetChanged();//notifies the recycler that the data has changed
     }
 }
